d5e683938975e2d03720cde25417a196a644c0eb
guoyabao <guoyabao886@163.com>
2025-11-28 09:14:13 +0800

    07-TitleCoordinateAxisConfig

---
+++ code/project/src/components/preview/07-TitleCoordinateAxisConfig/title-01.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="chart"></div>
+</template>
+<script lang="ts" setup>
+import { default as VChart } from "@visactor/vchart";
+import { onMounted } from "vue";
+
+// 实现A组和B组上半年的销售数据对比-Compare the sales data of A group and B group in recent years
+const spec = {
+  type: "bar",
+  data: [
+    {
+      id: "barData",
+      values: [
+        { type: "A", month: "01", value: 129 },
+        { type: "B", month: "01", value: 110 },
+        { type: "A", month: "02", value: 130 },
+        { type: "B", month: "02", value: 123 },
+        { type: "A", month: "03", value: 126 },
+        { type: "B", month: "03", value: 133 },
+        { type: "A", month: "04", value: 117 },
+        { type: "B", month: "04", value: 133 },
+        { type: "A", month: "05", value: 114 },
+        { type: "B", month: "05", value: 133 },
+        { type: "A", month: "06", value: 111 },
+        { type: "B", month: "06", value: 133 },
+      ],
+    },
+  ],
+  xField: ["month", "type"],
+  yField: "value",
+  seriesField: "type",
+  bar: {
+    style: {
+      cornerRadius: 4,
+    },
+  },
+  // 01-Title标题组件基础
+  title: {
+    text: "A组和B组销售数据对比", // 主标题文本
+    subtext: "2024年第一季度", // 副标题文本
+    visible: true, // 是否显示标题
+  },
+
+  // 02-Title标题布局与位置调整
+  // title: {
+  //   text: "销售数据对比",
+  //   orient: "top", // 标题位置：'top', 'bottom', 'left', 'right'
+  //   x: 100, // 标题左上角x坐标
+  //   y: 20, // 标题左上角y坐标
+  //   width: 400, // 标题宽度
+  //   height: 100, // 标题高度
+  //   align: "center", // 水平对齐方式：'left', 'center', 'right'
+  //   verticalAlign: "middle", // 垂直对齐方式：'top', 'middle', 'bottom'
+  //   innerPadding: 10, // 内边距
+  // },
+};
+onMounted(() => {
+  const vchart = new VChart(spec, {
+    dom: "chart",
+  });
+  vchart.renderSync();
+});
+</script>
--- code/project/src/views/HomeView.vue
+++ code/project/src/views/HomeView.vue
@@ -1,28 +1,27 @@
 <script setup lang="ts">
 // 课堂实操-Course practice
-import course01 from '../components/course/course-01.vue'
-import course02 from '../components/course/course-02.vue'
-import course03 from '../components/course/course-03.vue'
-import course04 from '../components/course/course-04.vue'
-import course05 from '../components/course/course-05.vue'
-import course06 from '../components/course/course-06.vue'
-
-
+import course01 from "../components/course/course-01.vue";
+import course02 from "../components/course/course-02.vue";
+import course03 from "../components/course/course-03.vue";
+import course04 from "../components/course/course-04.vue";
+import course05 from "../components/course/course-05.vue";
+import course06 from "../components/course/course-06.vue";
 
 // 课前演示-Preview before the course
 // import preview03 from '../components/preview/BasicConfig-03.vue'
-import preview04 from '../components/preview/LifeCycle-04.vue'
-import preview05 from '../components/preview/ChartComposition-05.vue'
-import preview06_FlattenedData from '../components/preview/06-DataTypesAndDefinitions/flattenedData-01.vue'
-import preview06_CsvData from '../components/preview/06-DataTypesAndDefinitions/csvData-02.vue'
-import preview06_TreeData from '../components/preview/06-DataTypesAndDefinitions/treeData-03.vue'
-import preview06_Transform1 from '../components/preview/06-DataTypesAndDefinitions/transforms1-04.vue'
-import preview06_Transform2 from '../components/preview/06-DataTypesAndDefinitions/transforms2-05.vue'
-import preview06_CaseStudy06 from '../components/preview/06-DataTypesAndDefinitions/caseStudy-06.vue'
+import preview04 from "../components/preview/LifeCycle-04.vue";
+import preview05 from "../components/preview/ChartComposition-05.vue";
+import preview06_FlattenedData from "../components/preview/06-DataTypesAndDefinitions/flattenedData-01.vue";
+import preview06_CsvData from "../components/preview/06-DataTypesAndDefinitions/csvData-02.vue";
+import preview06_TreeData from "../components/preview/06-DataTypesAndDefinitions/treeData-03.vue";
+import preview06_Transform1 from "../components/preview/06-DataTypesAndDefinitions/transforms1-04.vue";
+import preview06_Transform2 from "../components/preview/06-DataTypesAndDefinitions/transforms2-05.vue";
+import preview06_CaseStudy06 from "../components/preview/06-DataTypesAndDefinitions/caseStudy-06.vue";
+import preview07_Title01 from "../components/preview/07-TitleCoordinateAxisConfig/title-01.vue";
 </script>
 
 <template>
   <main>
-    <course06 />
+    <preview07_Title01 />
   </main>
 </template>
